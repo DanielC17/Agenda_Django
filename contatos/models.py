f7ba1c55cf955ad7b533ff7450b073688a2c3f05
Daniel Cabral <danielcabral.dev@gmail.com>
2022-05-02 15:48:28 -0300

    alterando o campo nome do models

--- contatos/models.py
+++ contatos/models.py
@@ -8,7 +8,7 @@ class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
 class Contato(models.Model):
-    nome = models.CharField(max_length=255)
+    nome = models.CharField(max_length=150)
     sobrenome = models.CharField(max_length=255, blank=True)
     telefone = models.CharField(max_length=255)
     email = models.CharField(max_length=255, blank=True)
