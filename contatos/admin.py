f1489ca7c9e973bb902c72509b88d42a8f69ce43
Daniel Cabral <danielcabral.dev@gmail.com>
2022-05-04 20:00:22 -0300

    exibindo todos  meus contatos da minha base da dados

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,8 +1,16 @@
+from csv import list_dialects
 from django.contrib import admin
 
 from .models import Categoria, Contato
 
 
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id','nome', 'sobrenome', 'telefone', 'email',
+    'categoria')
+
+    list_display_links = ('id', 'nome')
+
+
 admin.site.register(Categoria)
-admin.site.register(Contato)
+admin.site.register(Contato, ContatoAdmin)
 
--- contatos/views.py
+++ contatos/views.py
@@ -1,7 +1,11 @@
+import imp
 from django.shortcuts import render
+from .models import Contato
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'contatos/index.html')
+    contatos = Contato.objects.all() #selecionando os dados da minha base de dados. 
+    return render(request, 'contatos/index.html', {'contatos': contatos})
+
 
