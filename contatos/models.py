9b853145890187f639768261d9481a1246f63774
Daniel Cabral <danielcabral.dev@gmail.com>
2022-05-04 18:54:00 -0300

    Configurando a parte Admin

--- contatos/models.py
+++ contatos/models.py
@@ -7,6 +7,9 @@ from django.forms import CharField
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nome
+
 class Contato(models.Model):
     nome = models.CharField(max_length=150)
     sobrenome = models.CharField(max_length=255, blank=True)
@@ -15,6 +18,14 @@ class Contato(models.Model):
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+
+    def __str__(self):
+        return self.nome
+
+        
+
+
 
 
 
